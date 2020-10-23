25bc86b0b0132aa4dada594c5d88d6c406a46b47
thetayloredman <nutdriver716@gmail.com>
2020-10-23 13:48:06 -0700

    aaaa

---
+++ commands/ping.ts
@@ -0,0 +1,52 @@
+// DFur
+// By BadBoyHaloCat
+// Very owo code i have here~
+
+// +-------------------------------------------------+
+// | LEGAL STUFF                        not very owo |
+// |=================================================|
+// | +---------------------------------------------+ |
+// | | COPYRIGHT                                   | |
+// | |=============================================| |
+// | | Copyright (C) 2020  Logan Devine            | |
+// | |                                             | |
+// | | DFur is licensed under exclusive            | |
+// | | copyright. You may NOT use this source      | |
+// | | code (the "Bot") for anything other than    | |
+// | | usage in non-source form.                   | |
+// | |                                             | |
+// | | Unless the developers ("We") decide to      | |
+// | | remove the copyright on this code and       | |
+// | | place it under an applicable Open-source*   | |
+// | | licence, you may NOT use this codebase.     | |
+// | |                                             | |
+// | | The only permitted users of this codebase   | |
+// | | are the APPROVED** developers of this code. | |
+// | |                                             | |
+// | | *  Open-source describes any code licensed  | |
+// | |    under an applicable open licence such as | |
+// | |    the GNU GPL v3.0. Any licence that       | |
+// | |    allows others to modify and create       | |
+// | |    derivative works off of the Software is  | |
+// | |    considered Open-source.                  | |
+// | |                                             | |
+// | | ** Approved indicates a developer that has  | |
+// | |    invited to contribute to this codebase   | |
+// | |    and is not applicable to this copyright. | |
+// | +---------------------------------------------+ |
+// +-------------------------------------------------+
+
+// Modules
+import discord from 'discord.js';
+import chalk from 'chalk';
+
+// Main
+export function run(client: discord.Client, message: discord.Message, args: string[]) {
+    message.reply('pong!!!');
+}
+
+// Config
+export const config = {
+    name: 'ping',
+    description: 'Does stuff'
+}
