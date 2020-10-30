1cf0e30118fd1ab97318fead69d5ad8037d7d1be
thetayloredman <nutdriver716@gmail.com>
2020-10-30 10:42:16 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -65,6 +65,7 @@ client.cooldowns = new enmap({ name: 'cooldowns' });
 client.tildes = new enmap({ name: 'tildes' });
 client.owos = new enmap({ name: 'owos' });
 client.uwus = new enmap({ name: 'uwus' });
+client.ustats = new enmap({ name: 'ustats' });
 
 // in memory dbs
 client.commands = new enmap();
@@ -194,6 +195,7 @@ client.on('message', (message: discord.Message) => {
         }
 
         const commandExec: (client: discord.Client, message: discord.Message, args: string[], log: (mode: 'i'|'w'|'e', message: string) => void) => void|undefined = client.commands.get(command)?.run;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const commandConfig: any = client.commands.get(command)?.config;
         if (!commandExec) {
             // exit
