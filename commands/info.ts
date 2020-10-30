ff5963b99612cf940a25829a91ee95c9a66f0ba9
thetayloredman <nutdriver716@gmail.com>
2020-10-30 11:00:35 -0700

    Create info.ts

---
+++ commands/info.ts
@@ -0,0 +1,81 @@
+// ProtoBot
+// By BadBoyHaloCat
+// Very owo code i have here~
+
+// +-------------------------------------------------+
+// | LEGAL STUFF                        not very owo |
+// |=================================================|
+// | +---------------------------------------------+ |
+// | | COPYRIGHT                                   | |
+// | |=============================================| |
+// | | Copyright (C) 2020  Logan Devine            | |
+// | |                                             | |
+// | | ProtoBot is licensed under exclusive        | |
+// | | copyright. You may NOT use this source      | |
+// | | code (the "Bot") for anything other than    | |
+// | | usage in non-source form.                   | |
+// | |                                             | |
+// | | Unless the developers ("We") decide to      | |
+// | | remove the copyright on this code and       | |
+// | | place it under an applicable Open-source*   | |
+// | | licence, you may NOT use this codebase.     | |
+// | |                                             | |
+// | | The only permitted users of this codebase   | |
+// | | are the APPROVED** developers of this code. | |
+// | |                                             | |
+// | | *  Open-source describes any code licensed  | |
+// | |    under an applicable open licence such as | |
+// | |    the GNU GPL v3.0. Any licence that       | |
+// | |    allows others to modify and create       | |
+// | |    derivative works off of the Software is  | |
+// | |    considered Open-source.                  | |
+// | |                                             | |
+// | | ** Approved indicates a developer that has  | |
+// | |    invited to contribute to this codebase   | |
+// | |    and is not applicable to this copyright. | |
+// | +---------------------------------------------+ |
+// +-------------------------------------------------+
+
+// Modules
+import discord from 'discord.js';
+import chalk from 'chalk';
+
+// Interfaces, owo
+interface Client extends discord.Client {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [key: string]: any
+}
+
+// Main
+export function run(client: Client, message: discord.Message, args: string[], log: (mode: 'i'|'w'|'e', message: string) => void): void {
+    let userID: string|undefined;
+    if (!args[0]) {
+        userID = message.author.id;
+    } else if (/<@!?.+>/.test(args[0])) {
+        userID = args[0].replace(/[<@!>]/g, '');
+    } else {
+        userID = args[0];
+    }
+
+    if (!client.ustats.get(userID)) {
+        message.reply('I haven\'t ever seen that user! Maybe they have not talked before?');
+        return;
+    } else {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const uStats: { [key: string]: any } = client.ustats.get(userID);
+        message.reply(JSON.stringify(uStats));
+    }
+}
+
+// Config
+export const config = {
+    name: 'info',
+    description: 'Get a user\'s stats!',
+    enabled: false,
+    
+    // To restrict the command, change the "false" to the following
+    // format:
+    // 
+    // restrict: { users: [ "array", "of", "authorized", "user", "IDs" ] }
+    restrict: false
+}
