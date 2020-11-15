0da506b460e25deffea5891a146ffbbb71590d56
thetayloredman <nutdriver716@gmail.com>
2020-11-15 10:12:52 -0800

    Create help.ts

---
+++ commands/help.ts
@@ -0,0 +1,81 @@
+// ProtoBot
+// By BadBoyHaloCat
+// Very owo code i have here~
+
+// +-------------------------------------------------+
+// | LEGAL STUFF                        not very owo |
+// |=================================================|
+// | +---------------------------------------------+ |
+// | | COPYRIGHT                                   | |
+// | |=============================================| |
+// | | Copyright (C) 2020  BadBoyHaloCat           | |
+// | |                                             | |
+// | | ProtoBot is licensed under exclusive        | |
+// | | copyright. You may NOT use this source      | |
+// | | code (the "Bot") for anything other than    | |
+// | | usage in non-source form.                   | |
+// | |                                             | |
+// | | Unless the developers ("We") decide to      | |
+// | | remove the copyright on this code and       | |
+// | | place it under an applicable Open-source*   | |
+// | | licence, you may NOT use this codebase.     | |
+// | |                                             | |
+// | | The only permitted users of this codebase   | |
+// | | are the APPROVED** developers of this code. | |
+// | |                                             | |
+// | | *  Open-source describes any code licensed  | |
+// | |    under an applicable open licence such as | |
+// | |    the GNU GPL v3.0. Any licence that       | |
+// | |    allows others to modify and create       | |
+// | |    derivative works off of the Software is  | |
+// | |    considered Open-source.                  | |
+// | |                                             | |
+// | | ** Approved indicates a developer that has  | |
+// | |    invited to contribute to this codebase   | |
+// | |    and is not applicable to this copyright. | |
+// | +---------------------------------------------+ |
+// +-------------------------------------------------+
+
+// Modules
+import discord from 'discord.js';
+import chalk from 'chalk';
+
+// Interfaces, owo
+interface Client extends discord.Client {
+    [key: string]: any;
+}
+
+// Main
+export function run(
+    client: Client,
+    message: discord.Message,
+    args: string[],
+    log: (mode: 'i' | 'w' | 'e', message: string) => void
+): void {
+    log('i', "Giving help command info!");
+    const embed = new discord.MessageEmbed()
+        .setTitle('ProtoBot Help')
+        .setAuthor('ProtoBot')
+        .setTimestamp()
+        .setFooter(`Requested by ${message.author.tag}`)
+        .setDescription('Here are all of my commands!')
+    
+    client.commandsConfig.forEach((command) => {
+        embed.addField(command.name, `${command.description}${command.enabled ? '' : ' **[Disabled]**'}${command.restrict ? ' **[Restricted]**' : ''}`);
+    });
+
+    message.channel.send(embed);
+}
+
+// Config
+export const config = {
+    name: 'help',
+    description: 'List all available commands!',
+    enabled: true,
+
+    // To restrict the command, change the "false" to the following
+    // format:
+    //
+    // restrict: { users: [ "array", "of", "authorized", "user", "IDs" ] }
+    restrict: false
+};
