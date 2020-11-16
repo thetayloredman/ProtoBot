6fc970dea59e63c4c31f9688d38f5270106c814f
thetayloredman <nutdriver716@gmail.com>
2020-11-16 08:27:01 -0800

    restyle all files

--- commands/help.ts
+++ commands/help.ts
@@ -1,17 +1,17 @@
 /**
  * ProtoBot -- A Discord furry bot
  * Copyright (C) 2020  BadBoyHaloCat
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -32,17 +32,30 @@ export function run(
     args: string[],
     log: (mode: 'i' | 'w' | 'e', message: string) => void
 ): void {
-    log('i', "Giving help command info!");
+    log('i', 'Giving help command info!');
     const embed = new discord.MessageEmbed()
         .setTitle('ProtoBot Help')
         .setAuthor('ProtoBot')
         .setTimestamp()
         .setFooter(`Requested by ${message.author.tag}`)
-        .setDescription('Here are all of my commands!')
-    
-    client.commandsConfig.forEach((command: { name: string, description: string, enabled: boolean, restrict: boolean|{users:string[]} }) => {
-        embed.addField(command.name, `${command.description}${command.enabled ? '' : ' **[Disabled]**'}${command.restrict ? ' **[Restricted]**' : ''}`, true);
-    });
+        .setDescription('Here are all of my commands!');
+
+    client.commandsConfig.forEach(
+        (command: {
+            name: string;
+            description: string;
+            enabled: boolean;
+            restrict: boolean | { users: string[] };
+        }) => {
+            embed.addField(
+                command.name,
+                `${command.description}${
+                    command.enabled ? '' : ' **[Disabled]**'
+                }${command.restrict ? ' **[Restricted]**' : ''}`,
+                true
+            );
+        }
+    );
 
     message.channel.send(embed);
 }
