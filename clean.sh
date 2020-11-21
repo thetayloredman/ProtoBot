cd7dd0945457e8a66b199019ad7ad7cc8292c1f8
BadBoyHaloCat <nutdriver716@gmail.com>
2020-11-21 09:49:07 -0800

    Add Error SourceMap Support (#42)
    * Support source maps
    
    Source maps are now generated with the 'tsc' compiler, so
    we can get proper TS code-type errors in ^Cr stack traces.
    
    * Support SourceMaps in gitignore and cleaner
    
    * Update clean.sh
    
    * Fix compilation errors...
    
    * Reformatted with Prettier
    
    * Fix broken prettier, wow
    
    * Fix build script while we're at it
    
    Co-authored-by: thetayloredman <thetayloredman@users.noreply.github.com>

--- clean.sh
+++ clean.sh
@@ -16,22 +16,48 @@ case "$isSure" in
         # user said ok
         echo "Clearing ./commands/*.js";
         rm -fv ./commands/*.js;
+        echo "Clearing ./commands/*.js.map";
+        rm -fv ./commands/*.js.map;
         echo "Clearing ./modules/*.js";
         rm -fv ./modules/*.js;
+        echo "Clearing ./modules/*.js.map";
+        rm -fv ./modules/*.js.map;
         echo "Clearing ./index.js";
         rm -fv ./index.js;
+        echo "Clearing ./index.js.map";
+        rm -fv ./index.js.map
         echo "Clearing ./template*.js";
         rm -fv ./template*.js;
+        echo "Clearing ./template*.js.map";
+        rm -fv ./template*.js.map;
         echo "Clearing ./config.js";
         rm -fv ./config.js;
+        echo "Clearing ./config.js.map";
+        rm -fv ./config.js.map;
         echo "Clearing ./config.rename-me.js";
         rm -fv ./config.rename-me.js;
+        echo "Clearing ./config.rename-me.js.map";
+        rm -fv ./config.rename-me.js.map;
         echo "Clearing ./log.js";
         rm -fv ./log.js;
+        echo "Clearing ./log.js.map";
+        rm -fv ./log.js.map;
+        echo "Clearing ./Client.js";
+        rm -fv ./Client.js;
+        echo "Clearing ./Client.js.map";
+        rm -fv ./Client.js.map;
+        echo "Clearing ./header.js";
+        rm -fv ./header.js;
+        echo "Clearing ./header.js.map";
+        rm -fv ./header.js.map;
         echo "All cleaned! Have a nice day!";
         ;;
-    *)
-        echo "You need to type 'y'!";
+    [nN][oO]|[nN])
+        echo "You rejected.";
         exit 1;
+        ;;
+    *)
+        echo "Unknown input $isSure";
+        exit 2;
         ;;
 esac
