4982668d98788d5ea90264b1ed60cd639248e12b
thetayloredman <nutdriver716@gmail.com>
2020-11-22 09:01:23 -0800

    Fix conflict with Prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
     ],
     extends: [
         'eslint:recommended',
-        'plugin:@typescript-eslint/recommended'
+        'plugin:@typescript-eslint/recommended',
+        'prettier'
     ],
     rules: {
         /* ======================================= */
