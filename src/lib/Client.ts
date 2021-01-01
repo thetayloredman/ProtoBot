7a7df8eb711634b37f787d4f4708ca0a1112808b
thetayloredman <nutdriver716@gmail.com>
2021-01-01 15:27:24 -0800

    remove un-needed ignores because enmap updates

--- src/lib/Client.ts
+++ src/lib/Client.ts
@@ -30,26 +30,16 @@ export default class Client extends BaseClient {
             USER_STATS: { hugs: 0 },
             COOLDOWNS: { owos: 0, uwus: 0, tildes: 0 }
         };
-        // FIXME: type problems
-        // @ts-ignore
         this.cooldowns = new enmap({ name: 'cooldowns', verbose: (q: string) => EnmapVerbose('cooldowns', q) });
-        // @ts-ignore
         this.tildes = new enmap({ name: 'tildes', verbose: (q: string) => EnmapVerbose('tildes', q) });
-        // @ts-ignore
         this.owos = new enmap({ name: 'owos', verbose: (q: string) => EnmapVerbose('owos', q) });
-        // @ts-ignore
         this.uwus = new enmap({ name: 'uwus', verbose: (q: string) => EnmapVerbose('uwus', q) });
-        // @ts-ignore
         this.ustats = new enmap({ name: 'ustats', verbose: (q: string) => EnmapVerbose('ustats', q) });
-        // @ts-ignore
         this.uconfs = new enmap({ name: 'uconfs', verbose: (q: string) => EnmapVerbose('uconfs', q) });
-        // @ts-ignore
         this.markovMessages = new enmap({ name: 'markovMessages', verbose: (q: string) => EnmapVerbose('markovMessages', q) });
-        // @ts-ignore
         this.fursonas = new enmap({ name: 'fursonas', verbose: (q: string) => EnmapVerbose('fursonas', q) });
 
         // In memory items
-        // FIXME: doesn't have verbose
         this.commands = new enmap();
         this.commandsConfig = new enmap();
         this.commandsRefs = new enmap(); // Refs are basically aliases that "link" to the actual command
