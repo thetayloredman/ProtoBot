e9027f37eda035c02505d1625dc47a5f016c7810
thetayloredman <nutdriver716@gmail.com>
2021-01-02 17:18:05 -0800

    feat: add memory stats

--- src/index.ts
+++ src/index.ts
@@ -35,6 +35,9 @@ import Client from '@lib/Client';
 import discord from 'discord.js';
 import type Command from '@lib/interfaces/Command';
 
+// MemStat Auto
+import '@lib/memstatAuto';
+
 // Log import
 import log from './log';
 
